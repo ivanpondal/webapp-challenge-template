01376a3759650190e31eca2b4f7d0949782fda8a
Iván Pondal <ivan.pondal@gmail.com>
2024-11-20 19:08:33 -0300

    Add avatar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { LaunchItem } from "@/components/launches/LaunchItem";
+import { SpaceLaunchItem } from "@/components/space-launch/SpaceLaunchItem";
 
 export default function Home() {
   return (
     <div className="container sm mx-auto mt-4">
       <h1 className="text-4xl font-bold">martian chronicles</h1>
       <div className="container mt-4 max-w-md">
-        <LaunchItem />
+        <SpaceLaunchItem name="FalconSat" successful={true} date={new Date()} patchImageUrl="https://images2.imgbox.com/94/f2/NN6Ph45r_o.png" />
       </div>
     </div>
   );
