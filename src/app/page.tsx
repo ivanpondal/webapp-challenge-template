c6fbf98f65a631f3b364b089eb66eef3defbf81c
Iván Pondal <ivan.pondal@gmail.com>
2024-11-20 18:25:33 -0300

    Add card and launch item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { LaunchItem } from "@/components/launches/LaunchItem";
+
 export default function Home() {
   return (
-    <div className="container mx-auto md">
+    <div className="container sm mx-auto mt-4">
       <h1 className="text-4xl font-bold">martian chronicles</h1>
+      <div className="container mt-4 max-w-md">
+        <LaunchItem />
+      </div>
     </div>
   );
 }
